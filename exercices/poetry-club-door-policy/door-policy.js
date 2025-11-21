3fb7dd2136e3efbbaf425c217cf4cc85b05fb25c
latifcodess <196327957+LatifKrasniqi24@users.noreply.github.com>
2025-11-21 14:03:35 +0100

    feat(poetry-club-door-policy): exercice corrigé
    [1] [DONE]

--- exercices/poetry-club-door-policy/door-policy.js
+++ exercices/poetry-club-door-policy/door-policy.js
@@ -38,7 +38,7 @@ export function frontDoorResponse(line) {
  * @returns {string} the front door password
  */
 export function frontDoorPassword(word) {
-  return word[0].toUpperCase() + '' + word.slice(1).toLowerCase();
+  return word[0].toUpperCase() + word.slice(1).toLowerCase();
 }
 
 /**
@@ -62,5 +62,5 @@ export function backDoorResponse(line) {
  * @returns {string} the back door password
  */
 export function backDoorPassword(word) {
-  return word[0].toUpperCase() + '' + word.slice(1).toLowerCase() + '' + ", please"
+  return word[0].toUpperCase() + word.slice(1).toLowerCase() + ", please"
 }
