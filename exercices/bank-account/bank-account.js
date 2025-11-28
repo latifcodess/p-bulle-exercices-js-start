d1d0583b6410cda6198133cb095f7cf4c26f2511
latifcodess <196327957+LatifKrasniqi24@users.noreply.github.com>
2025-11-28 13:27:51 +0100

    feat(bank-account): exercice finit
    [30] [DONE]

--- exercices/bank-account/bank-account.js
+++ exercices/bank-account/bank-account.js
@@ -4,28 +4,82 @@
 //
 
 export class BankAccount {
+  // Attributs
+  Balance;
+  isClosed;
+
   constructor() {
-    throw new Error('Remove this line and implement the function');
+    this.Balance = 0;
+    this.isClosed = true;
   }
 
   open() {
-    throw new Error('Remove this line and implement the function');
+    // si le compte est deja ouvert : erreur
+    if(this.isClosed == false){
+      throw new ValueError();
+    }
+    // sinon le compte s'ouvre et la Balance se met à 0
+    else{
+      this.isClosed = false;
+      this.Balance = 0;
+    }
+    
   }
 
   close() {
-    throw new Error('Remove this line and implement the function');
+    // si le compte est deja fermé : erreur
+    if(this.isClosed == true){
+      throw new ValueError();
+    }
+    // sinon on ferme le compte
+    else{
+      this.isClosed = true;
+    }
   }
 
-  deposit() {
-    throw new Error('Remove this line and implement the function');
+  deposit(depos) {
+    // si le compte est fermé : erreur
+    if(this.isClosed == true){
+      throw new ValueError();
+    }
+    else{
+      // si le depos est négative : erreur
+      if(depos < 0){
+        throw new ValueError();
+      }
+      // sinon on ajoute le depos à la Balance
+      else{
+        this.Balance += depos;
+      }      
+    }
   }
 
-  withdraw() {
-    throw new Error('Remove this line and implement the function');
+  withdraw(wdraw) {
+    // si le compte est fermé : erreur
+    if(this.isClosed == true){
+      throw new ValueError();
+    }
+    else {
+      // si wdraw est négatif ou qu'il est superieur à Balance : erreur
+      if (wdraw < 0 || wdraw > this.Balance){
+        throw new ValueError();
+      }
+      // sinon on enleve wdraw de Balance
+      else{
+        this.Balance -= wdraw;
+      }
+    }
   }
 
   get balance() {
-    throw new Error('Remove this line and implement the function');
+    // si le compte est fermé : erreur
+    if(this.isClosed == true){
+      throw new ValueError();
+    }
+    // sinon on retourne Balance
+    else{
+      return this.Balance;
+    }
   }
 }
 
