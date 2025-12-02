16b401c90c471101027a28b247448c717afebf5e
LatifKrasniqi24 <196327957+LatifKrasniqi24@users.noreply.github.com>
2025-12-02 11:13:56 +0100

    feat(bob): exercice terminé
    [120] [DONE]

--- exercices/bob/bob.js
+++ exercices/bob/bob.js
@@ -3,6 +3,28 @@
 // convenience to get you started writing code faster.
 //
 
-export const hey = (message) => {
-  throw new Error('Remove this line and implement the function');
+export const hey = (msg) => {
+  // verifie que le msg sans espaces finit par '?'
+  if (msg.trim().endsWith('?')) {
+    // verifie que le msg n'a pas de lettre minuscule et
+    //  qu'il y a que des lettre majuscules
+    if (!/[a-z]/.test(msg) && /[A-Z]/.test(msg)) {
+      return "Calm down, I know what I'm doing!";
+    }
+    return "Sure.";
+  }
+
+  // verifie si le msg contient au moins un caractère
+  if (/[a-zA-Z\d]/.test(msg)) {
+    // verifie que le msg n'a pas de lettre minuscule et
+    //  qu'il y a que des lettre majuscules
+    if (!/[a-z]/.test(msg) && /[A-Z]/.test(msg)) {
+      return "Whoa, chill out!";
+    } 
+    else{
+      return "Whatever.";
+    }
+  }
+  // retourne ceci si aucune de toute les conditions n'a été validée
+  return "Fine. Be that way!";
 };
